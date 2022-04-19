f51b9a64d94b2edd712949946ac173521ffe9ea4
lenargasimov <lenargasimov@duck.com>
2022-04-19 17:37:08 +0300

    feat: added Flatmate

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 class Bill:
+    """
+    Object that contains data about a bill,
+    such as total amount and period of the bill.
+    """
 
-    def __init__(self, amount):
+    def __init__(self, amount, period):
         self.amount = amount
+        self.period = period
 
+
+class Flatmate:
+    """
+    Creates a flatmate person who lives in the flat
+    and pays a share of the bill.
+    """
+
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill):
+        pass
+        
 
 bill = Bill(120)
